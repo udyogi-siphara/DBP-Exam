02d895d238a4fa11630416d0c01bcf69f0f24e1c
udyogi-siphara <udyogisiphara22@gmail.com>
2022-07-12 10:59:34 -0700

    Coding Student model and StudentTM

---
+++ src/controller/StudentCrudController.java
@@ -0,0 +1,13 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: DBPExam
+ * Date        : 7/12/2022
+ * Time        : 10:53 AM
+ */
+
+package controller;
+
+public class StudentCrudController {
+    
+
+}
--- src/model/Student.java
+++ src/model/Student.java
@@ -8,5 +8,82 @@
 package model;
 
 public class Student {
+    private String id;
+    private String name;
+    private String email;
+    private String telNum;
+    private String address;
+    private String nic;
 
+    public Student() {
+    }
+
+    public Student(String id, String name, String email, String telNum, String address, String nic) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.telNum = telNum;
+        this.address = address;
+        this.nic = nic;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(String telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", telNum='" + telNum + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }

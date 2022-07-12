a605bdd91043c264dced748213d47cd0190bcf97
udyogi-siphara <udyogisiphara22@gmail.com>
2022-07-12 09:33:51 -0700

    Create UI and Added Libraries

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -7,6 +7,35 @@
 
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentFormController {
 
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentContact;
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentNic;
+    public JFXButton btnAddStudent;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void btnAddStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuDeleteOnAction(ActionEvent actionEvent) {
+    }
 }

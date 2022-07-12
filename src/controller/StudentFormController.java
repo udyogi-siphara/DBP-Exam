f6140649f6b76182d0c06ca93704da8641079052
udyogi-siphara <udyogisiphara22@gmail.com>
2022-07-12 12:27:01 -0700

    Added delete Student

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -126,6 +126,13 @@ public class StudentFormController {
         btnAddStudent.setText("Update");
     }
 
-    public void menuDeleteOnAction(ActionEvent actionEvent) {
-    }
+    public void menuDeleteOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Student selectedItem = tblStudent.getSelectionModel().getSelectedItem();
+
+        if (CrudUtil.execute("DELETE FROM Student WHERE student_id = ?",selectedItem.getId())){
+            new Alert(Alert.AlertType.CONFIRMATION,"Deleted.").show();
+            loadAllStudents();
+        }else{
+            new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+        }    }
 }
